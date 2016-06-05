4b9968485f10db0e2feb35849012cfc2f8ccff0e
Jack Hanford <jackhanford@gmail.com>
2016-06-05 15:51:14 -0700

    update app

--- website/app.js
+++ website/app.js
@@ -4,9 +4,6 @@ angular.module('exampleApp', [
     require('../')
   ])
   .controller('controller', function ($scope) {
-    $scope.loaded = 'hello world'
-    $scope.open = false
-
     $scope.openNav = function () {
       $scope.$broadcast('openSidebar')
     }
